44dcd04f2495b92e019bd364cf77835ba40b6a88
Greta Lartigue <gretagostina@gmail.com>
2022-09-26 23:09:02 +0200

    sin bonos

---
+++ vue-project/src/components/ToDoList.vue
@@ -0,0 +1,14 @@
+<script setup>
+  defineProps({
+    msg2: {
+      type: String,
+      required: true
+    }
+  })
+  </script>
+<template>
+  <div>
+    <input type="checkbox" id="todo-item" />
+    <label for="todo-item">{{ msg2 }}</label>
+  </div>
+</template>
